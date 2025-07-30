5d32aea4baf8d5b96bc370d9747e6f143e08d600
Tim Kanarsky <tkanarsky@outlook.com>
2025-07-30 02:27:37 +0000

    try out efficientnetv2_s

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
-from torchvision.models import mobilenet_v2, MobileNet_V2_Weights
+from torchvision.models import efficientnet_v2_s, EfficientNet_V2_S_Weights
 from dataset import get_train_dataset
 
 class RotationClassfier(nn.Module):
     def __init__(self):
         super().__init__()
-        self.mobilenet_v2 = mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
-        self.mobilenet_v2.classifier = nn.Identity()
+        self.efficientnet_v2 = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights.DEFAULT)
+        self.efficientnet_v2.classifier = nn.Identity()
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.2, inplace=False),
             nn.Linear(in_features=1280, out_features=2048, bias=True),
@@ -16,12 +16,14 @@ class RotationClassfier(nn.Module):
         )
 
     def forward(self, x: torch.Tensor):
-        mobilenet_out = self.mobilenet_v2(x)
-        return self.classifier(mobilenet_out)
+        efficientnet_out = self.efficientnet_v2(x)
+        return self.classifier(efficientnet_out)
 
 if __name__ == "__main__":
     ds = get_train_dataset()
     rc = RotationClassfier()
     img = ds[0]['image']
     img = img[None, ...]
+    out = rc(img)
+    print(out.shape)
 
